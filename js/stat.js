4369b02528ab8232f1c4299e3437bc95c5860ef0
Alexander Zimin <klesh2@mail.ru>
2018-01-30 22:20:58 +0300

    Статистика игры - правка

--- js/stat.js
+++ js/stat.js
@@ -14,7 +14,7 @@ var BAR_X = 90;
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -26,14 +26,14 @@ var getMaxElement = function (arr) {
   }
 
   return maxElement;
-}
+};
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
-  ctx.font = "16px PT Mono";
+  ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP * 2);
 
@@ -48,4 +48,4 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(names[i], CLOUD_X + BAR_WIDTH * (i + 1) + BAR_GAP * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
     ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH * (i + 1) + BAR_GAP * i, BAR_X + BAR_HEIGHT - barTotalHeight - GAP);
   }
-}
+};
